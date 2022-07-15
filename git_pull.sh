60a72dad374708f19fb4191f5a1bc8c9af9da6b7
inflatus <inflatus@gmail.com>
2022-07-15 18:07:53 -0400

    updating file

--- git_pull.sh
+++ git_pull.sh
@@ -10,14 +10,14 @@
 #git pull bash_scripts
 echo "git pull bash_scripts"
 cd ~/Programming/bash_scripts || exit
-git pull 
+git pull >> bash_scripts
 
 #git pull python
 echo "git pull python"
 cd ~/Programming/python || exit
-git pull 
+git pull >> python
 
 #git pull tryhackme
 echo "git pull tryhackme"
 cd ~/Programming/tryhackme || exit
-git pull
+git pull >> tryhackme
