368121df06d44c08a474b6f7c2b3b0c6f653caab
inflatus <inflatus@gmail.com>
2022-07-15 17:46:33 -0400

    added a git pull script

---
+++ git_pull.sh
@@ -0,0 +1,22 @@
+!#/bin/bash
+
+#using this to git pull the following folders on my machine
+#bash_scripts
+#python
+#tryhackme
+
+#will add more folders as they are created
+
+#git pull bash_scripts
+echo "git pull bash_scripts"
+git pull ~/Programming/bash_scripts
+
+#git pull python
+echo "git pull python"
+git pull ~/Programming/python
+
+#git pull tryhackme
+echo "git pull tryhackme"
+git pull ~/Programming/tryhackme
+
+fi
