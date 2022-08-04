34c86b181866d99b34ad1f1a169c4045e1fe13c8
inflatus <inflatus@gmail.com>
2022-08-04 18:06:01 -0400

    added picoctf and main branch

--- git_pull.sh
+++ git_pull.sh
@@ -4,6 +4,7 @@
 #bash_scripts
 #python
 #tryhackme
+#picoctf
 
 #will add more folders as they are created
 
@@ -11,16 +12,22 @@
 echo "git pull bash_scripts"
 sleep 15
 cd ~/Programming/bash_scripts || exit
-git pull
+git pull origin main
 
 #git pull python
 echo "git pull python"
 sleep 15
 cd ~/Programming/python || exit
-git pull
+git pull origin main
 
 #git pull tryhackme
 echo "git pull tryhackme"
 sleep 15
 cd ~/Programming/tryhackme || exit
-git pull
+git pull origin main
+
+#git pull picoctf
+echo "git pull picoctf"
+sleep 15
+cd ~/Programming/picoctf || exit
+git pull origin main
